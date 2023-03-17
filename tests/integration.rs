5336300e0a7954584a0839e90e5bb4b887865352
Piotr Socha <piotrsocha01@gmail.com>
2023-03-17 12:15:58 +0100

    Change lib name

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use uwb_rs_defmt as _; // memory layout + panic handler
+use uwb_rs as _; // memory layout + panic handler
 
 // See https://crates.io/crates/defmt-test/0.3.0 for more documentation (e.g. about the 'state'
 // feature)
